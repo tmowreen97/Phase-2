d1d0da6b64ebe0afc4077a882d760c18044e8088
Tasreen Mowreen <tmowreen97@gmail.com>
2022-10-01 20:11:39 -0400

    Done

--- my-app/src/components/About.js
+++ my-app/src/components/About.js
@@ -6,7 +6,8 @@ export default function About(){
       <h3>Welcome to Simply Delicious!</h3>
       <p>This is where you can simply store all your delicious recipes.</p>
       <p>On the 'My Recipes' page, you have a display of all your stored recipes.</p>
-      <p>On the 'Add Recipe' page is where you can add a new recipe to your collection. You can add as many steps and ingredients you need, as well as add a picture to show your delicious creation!</p>
+      <p>On the 'Add Recipe' page is where you can add a new recipe to your collection. </p>
+      <p>You can add as many steps and ingredients you need, as well as add a picture to show your delicious creation!</p>
       <p className="lastLine">Lets get to cooking!</p>
     </div>
   )
